c239aff833c7285b9d0aeb74f737dc93398d9ccf
mura012 <murakoduai@gmail.com>
2023-02-03 16:34:15 +0900

    型を作成

--- src/types/todo.d.ts
+++ src/types/todo.d.ts
@@ -1,8 +1,10 @@
 export type Limit = "今日中" | "今週中" | "今月中" | "継続";
-export type Importance = "high" | "middle" | "low";
+export type Importance = "高" | "中" | "下";
 export type Todo = {
   task: string;
   limit: Limit;
   importance: Importance;
   isDone: boolean;
 };
+
+export type DatabaseType = Todo & { userId: string };
