aefd8797eee68d5cb129805d9d659eebf35e75cd
mura012 <murakoduai@gmail.com>
2023-02-05 15:42:28 +0900

    ログインしていないときに「タスクを追加」ボタンを押したときの挙動を変更

--- src/Components/header/Header.tsx
+++ src/Components/header/Header.tsx
@@ -1,13 +1,16 @@
+import { auth } from "@/lib/firebase";
 import { Modal } from "@mantine/core";
 import { Button } from "@mantine/core/";
 import { Group } from "@mantine/core/";
 import { useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
 import { Auth } from "../auth/auth";
 
 import { ModalContent } from "./modalContent";
 
 export const Header = () => {
   const [todoOpened, setTodoOpened] = useState<boolean>(false);
+  const [user] = useAuthState(auth);
   return (
     <header className="w-full h-20 text-black flex items-center justify-around">
       <p>hedda</p>
@@ -15,7 +18,7 @@ export const Header = () => {
         <Modal
           opened={todoOpened}
           onClose={() => setTodoOpened(false)}
-          title="タスクを追加"
+          title={user ? "タスクを追加" : "Googleアカウントでサインイン"}
           centered
         >
           <ModalContent />
--- src/Components/header/ModalContent.tsx
+++ src/Components/header/ModalContent.tsx
@@ -2,6 +2,8 @@ import { auth } from "@/lib/firebase";
 import { Input, Radio, Button } from "@mantine/core";
 import { ComponentProps, useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
+import { ModalSignInContent } from "../auth/authButton/modalSignInContent";
+import { SignIn } from "../auth/authButton/signIn";
 
 export const ModalContent = () => {
   const [task, setTask] = useState<string>("");
@@ -34,54 +36,58 @@ export const ModalContent = () => {
 
   return (
     <>
-      <form className="pb-10">
-        <label>
-          <span>タスク</span>
-          <Input.Wrapper description="20文字以内で入力してください">
-            <Input
-              placeholder="勉強"
+      {user ? (
+        <form className="pb-10">
+          <label>
+            <span>タスク</span>
+            <Input.Wrapper description="20文字以内で入力してください">
+              <Input
+                placeholder="勉強"
+                className="mb-3"
+                value={task}
+                onChange={(e) => setTask(e.target.value)}
+              />
+            </Input.Wrapper>
+            <p className="mb-1 -mt-2 text-right">{task.length}/20</p>
+          </label>
+          <label>
+            <span>リミット</span>
+            <Radio.Group
+              description="このタスクはいつまでに行わなければいけませんか？"
               className="mb-3"
-              value={task}
-              onChange={(e) => setTask(e.target.value)}
-            />
-          </Input.Wrapper>
-          <p className="mb-1 -mt-2 text-right">{task.length}/20</p>
-        </label>
-        <label>
-          <span>リミット</span>
-          <Radio.Group
-            description="このタスクはいつまでに行わなければいけませんか？"
-            className="mb-3"
-            value={limit}
-            onChange={setLimit}
+              value={limit}
+              onChange={setLimit}
+            >
+              <Radio value="今日中" label="今日中" />
+              <Radio value="今週中" label="今週中" />
+              <Radio value="今月中" label="今月中" />
+              <Radio value="継続" label="継続" />
+            </Radio.Group>
+          </label>
+          <label>
+            <span>優先度</span>
+            <Radio.Group
+              description="このタスクの優先度はどれくらいですか？"
+              defaultValue="高"
+              value={importance}
+              onChange={setImportance}
+            >
+              <Radio value="高" label="高" />
+              <Radio value="中" label="中" />
+              <Radio value="低" label="低" />
+            </Radio.Group>
+          </label>
+          <Button
+            className="absolute right-8 bottom-3"
+            onClick={createTask}
+            disabled={task && task.length <= 20 ? false : true}
           >
-            <Radio value="今日中" label="今日中" />
-            <Radio value="今週中" label="今週中" />
-            <Radio value="今月中" label="今月中" />
-            <Radio value="継続" label="継続" />
-          </Radio.Group>
-        </label>
-        <label>
-          <span>優先度</span>
-          <Radio.Group
-            description="このタスクの優先度はどれくらいですか？"
-            defaultValue="高"
-            value={importance}
-            onChange={setImportance}
-          >
-            <Radio value="高" label="高" />
-            <Radio value="中" label="中" />
-            <Radio value="低" label="低" />
-          </Radio.Group>
-        </label>
-        <Button
-          className="absolute right-8 bottom-3"
-          onClick={createTask}
-          disabled={task && task.length <= 20 ? false : true}
-        >
-          追加
-        </Button>
-      </form>
+            追加
+          </Button>
+        </form>
+      ) : (
+        <ModalSignInContent />
+      )}
     </>
   );
 };
