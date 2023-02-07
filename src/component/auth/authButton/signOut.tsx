f50ac39bfdb0f00f1e8c65d4b0fbed92c8e93e94
mura012 <murakoduai@gmail.com>
2023-02-07 15:43:47 +0900

    タスクを全部削除する機能を実装

--- src/component/auth/authButton/signOut.tsx
+++ src/component/auth/authButton/signOut.tsx
@@ -1,23 +1,49 @@
 import { auth } from "@/lib/firebase";
 import { Button } from "@mantine/core";
-import { Dispatch, SetStateAction } from "react";
+import { ComponentProps, Dispatch, SetStateAction } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 export const SignOut = ({
   setProfileOpened,
 }: {
   setProfileOpened: Dispatch<SetStateAction<boolean>>;
 }) => {
+  const [user] = useAuthState(auth);
   const handleSignOut = () => {
     setProfileOpened(false);
     auth.signOut();
+  };
+  const handleAllDelete: ComponentProps<"button">["onClick"] = async (e) => {
+    e.preventDefault();
+    try {
+      await fetch(`${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/tasks/all`, {
+        method: "delete",
+        // ↓忘れていたので注意
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          userId: user?.email,
+        }),
+      });
+      window.location.reload();
+    } catch (err) {
+      console.log(err);
+    }
   };
   return (
     <>
       <Button
         onClick={handleSignOut}
-        className=" text-black hover:bg-blue-300 bg-white"
+        className=" text-black hover:bg-blue-300 bg-white w-full"
       >
         <p className="m-0">サインアウト</p>
+      </Button>
+      <Button
+        onClick={handleAllDelete}
+        className=" text-black hover:bg-blue-300 bg-white w-full"
+      >
+        <p className="m-0">タスクをすべて削除</p>
       </Button>
     </>
   );
